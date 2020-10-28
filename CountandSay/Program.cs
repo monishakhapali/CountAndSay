9164e59585ba5741da801b18cfa8a9808aefc922
Monisha Khapali <monisha.khapali@logicsolutions.com>
2020-10-28 19:36:14 -0400

    Created count&say

---
+++ CountandSay/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace CountandSay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int n = 4;
+            string r = "1";
+
+            while (n != 0)
+            {
+               
+                char prev = char.MinValue;
+                foreach (char c in r)
+                {
+                    
+                    int freq = r.Count(f => f == c);
+                    if(r=="1")
+                    {
+                        r = freq.ToString() + r;
+                    }
+                    else
+                    {
+                        if(c!= prev)
+                        {
+                            r = freq.ToString() + c;
+                            prev = c;
+                           
+                        }
+                    }
+
+                }
+                n--;
+            }
+        }
+    }
+}
